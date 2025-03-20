d5f257b409feeae9ee3f2eebbb81984e6e61abfd
Aman Thakor <123771620+athakor-se@users.noreply.github.com>
2025-03-20 15:33:31 +0000

    SmalletPositiveMissingNumber

---
+++ src/other/SmalletPositiveMissingNumber.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class SmalletPositiveMissingNumber {
+
+    public static int getSmallestMissingPositive(int arr[]) {
+        if (arr == null || arr.length == 0) {
+            return 1;
+        }
+
+        var minPositive = 1;
+        var uniqueElements = Arrays.stream(arr).boxed().collect(Collectors.toSet());
+        while (uniqueElements.contains(minPositive)) {
+            minPositive++;
+        }
+        return minPositive;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SmalletPositiveMissingNumber.getSmallestMissingPositive(new int[]{1, 2, 4, 5}));
+        System.out.println(SmalletPositiveMissingNumber.getSmallestMissingPositive(new int[]{3, 7, 1, 2, 8, 4, 5}));
+        System.out.println(SmalletPositiveMissingNumber.getSmallestMissingPositive(new int[]{1}));
+    }
+
+}
